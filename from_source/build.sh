153a31c65e42c4f127d0b66cc626551ef88ba078
FrancescoRuscelli <francescoruscelli24@gmail.com>
2025-02-17 18:13:18 +0100

    updated from source

--- from_source/build.sh
+++ from_source/build.sh
@@ -2,6 +2,5 @@
 
 IMAGE=horizon
 
-docker build --build-arg CACHE_DATE="$(date)"--rm -t ${IMAGE} .
-
-
+DOCKER_BUILDKIT=1 docker build --build-arg CACHE_DATE="$(date)"--rm -t ${IMAGE} . #--progress=plain --no-cache
+    
--- from_source/run.sh
+++ from_source/run.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 IMAGE=horizon
+NAME=borealis
 
-docker run -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix --env=DISPLAY --network="host" ${IMAGE}
-#docker run -it --rm --privileged --net="host" --env=NVIDIA_VISIBLE_DEVICES=all --env=NVIDIA_DRIVER_CAPABILITIES=all --env=DISPLAY --env=QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix --gpus all ${IMAGE} /bin/bash
-#docker run -it --rm --privileged --net=host --env=NVIDIA_VISIBLE_DEVICES=all --env=NVIDIA_DRIVER_CAPABILITIES=all --env=DISPLAY --env=QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=0 ${IMAGE} /bin/bash
+#  --privileged \
+#             --runtime=nvidia --gpus all \
+#             --env NVIDIA_DRIVER_CAPABILITIES=all \
+#             -v /dev/input:/dev/input \
+#             -v /tmp/.X11-unix:/tmp/.X11-unix \
+
+docker run -it --rm \
+            --env=DISPLAY \
+            --name ${NAME} ${IMAGE}
+            # --network="host" \
